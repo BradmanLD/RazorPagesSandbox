f26985dfb8ae68e5d34eec83448b4c9ec97f5784
Bradman Davis <bradman.davis@my.jcu.edu.au>
2020-07-07 10:45:09 +1000

    Add conversion to 2d array

--- ASCIIImageConverter/ASCII Image Converter.py
+++ ASCIIImageConverter/ASCII Image Converter.py
@@ -1,7 +1,15 @@
 from PIL import Image
+import numpy as np
 
 def main():
-    im = Image.open("testImage.jpg")
+    im = Image.open("smallTestImage.png")
     print(im.format, im.size, im.mode)
+
+    imageData = np.asarray(im)
+    print("Iterating through pixel contents:")
+    for x in range(len(imageData)):
+        for y in range(len(imageData[x])):
+            pixel = imageData[x][y]
+            print(pixel)
 
 main()
