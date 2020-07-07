0ba5cf4cf1d885f870a5abb688e27b9c0a0b0129
Bradman Davis <bradman.davis@my.jcu.edu.au>
2020-07-07 11:23:10 +1000

    Refactor pixel matrix conversion into function & refactor conversion to be more efficent

--- ASCIIImageConverter/ASCII Image Converter.py
+++ ASCIIImageConverter/ASCII Image Converter.py
@@ -5,11 +5,13 @@ def main():
     im = Image.open("smallTestImage.png")
     print(im.format, im.size, im.mode)
 
-    imageData = np.asarray(im)
-    print("Iterating through pixel contents:")
-    for x in range(len(imageData)):
-        for y in range(len(imageData[x])):
-            pixel = imageData[x][y]
+    pixelMatrix = get_pixel_matrix(im)
+    for row in pixelMatrix:
+        for pixel in row:
             print(pixel)
+
+def get_pixel_matrix(img):
+    pixels = list(img.getdata())
+    return [pixels[i:i+img.width] for i in range(0, len(pixels), img.width)]
 
 main()
